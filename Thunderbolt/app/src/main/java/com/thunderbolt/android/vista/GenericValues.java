63f6b057ccdaff869be8d245643c961d5e939d4a
carlos torres <carlosandrestorres30@gmail.com>
2016-11-24 00:34:14 -0400

    Valoresgenericos de enums

---
+++ Thunderbolt/app/src/main/java/com/thunderbolt/android/vista/GenericValues.java
@@ -0,0 +1,15 @@
+package com.thunderbolt.android.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thunderbolt.android.R;
+
+public class GenericValues extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic_values);
+    }
+}
