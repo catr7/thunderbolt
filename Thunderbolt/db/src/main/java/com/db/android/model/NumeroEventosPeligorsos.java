a33c36c700d1e1120dcbec637fd7a0bfcf9699e4
catr7 <carlosandrestorres30@gmail.com>
2016-11-23 19:59:15 -0430

    se empieza modelo

--- Thunderbolt/db/src/main/java/com/db/android/model/NumeroEventosPeligorsos.java
+++ Thunderbolt/db/src/main/java/com/db/android/model/NumeroEventosPeligorsos.java
@@ -1,8 +1,30 @@
 package com.db.android.model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by Andres y Jess on 21/11/2016.
  */
-
+@DatabaseTable(tableName="numeroEventosPeligrosos")
 public class NumeroEventosPeligorsos {
+
+    @DatabaseField(generatedId = true, allowGeneratedIdInsert = true)
+    private Long id;
+    @DatabaseField(foreign = true, foreignAutoCreate = true , foreignAutoRefresh = true, columnName = "fk_dimension_estructura")
+     private DimensionesEstructura  dimensionesEstructura;
+    estructura_en_evaluacion
+            enrutamiento_de_acometida
+    transformador_en_acometida
+            ambiente
+    longitud_de_la_acometida
+    @DatabaseField
+     private float calculo_np;
+    @DatabaseField
+    private float calculo_nm;
+    @DatabaseField
+    private float calculo_nl;
+    @DatabaseField
+    private float calculo_ni;
+
 }
