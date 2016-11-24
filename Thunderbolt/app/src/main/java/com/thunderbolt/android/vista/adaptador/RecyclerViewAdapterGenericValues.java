551709850e7ffc3c46571e8d821873994541088a
carlos torres <carlosandrestorres30@gmail.com>
2016-11-24 00:33:25 -0400

    Valoresgenericos de enums

---
+++ Thunderbolt/app/src/main/java/com/thunderbolt/android/vista/adaptador/RecyclerViewAdapterGenericValues.java
@@ -0,0 +1,46 @@
+package com.thunderbolt.android.vista.adaptador;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thunderbolt.android.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Carlos Torres on 23/11/16.
+ */
+public class RecyclerViewAdapterGenericValues extends RecyclerView.Adapter<ViewHolderGenericValues> {
+
+    private Map<String, String[]> enumSeleccionado = new HashMap<>();
+    private List<String> titulosEnum = new ArrayList<>();
+
+    public RecyclerViewAdapterGenericValues(Map<String, String[]> enumSeleccionado, List<String> titulosEnum) {
+        this.enumSeleccionado = enumSeleccionado;
+        this.titulosEnum = titulosEnum;
+    }
+
+    @Override
+    public ViewHolderGenericValues onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cv_generic_values, parent, false);
+        ViewHolderGenericValues pvh = new ViewHolderGenericValues(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderGenericValues holder, int position) {
+        holder.tituloEnum.setText(this.titulosEnum.get(position));
+        holder.descripcionEnum.setText(this.enumSeleccionado.get(this.titulosEnum.get(position))[0]);
+        holder.valorEnum.setText(this.enumSeleccionado.get(this.titulosEnum.get(position))[1]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titulosEnum.size();
+    }
+}
---
+++ Thunderbolt/app/src/main/java/com/thunderbolt/android/vista/adaptador/ViewHolderGenericValues.java
@@ -0,0 +1,29 @@
+package com.thunderbolt.android.vista.adaptador;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.thunderbolt.android.R;
+
+/**
+ * Created by Carlos Torres on 23/11/16.
+ */
+public class ViewHolderGenericValues extends RecyclerView.ViewHolder {
+
+    CardView cv;
+    TextView tituloEnum;
+    TextView valorEnum;
+    TextView descripcionEnum;
+
+    public ViewHolderGenericValues(View itemView) {
+        super(itemView);
+        cv = (CardView) itemView.findViewById(R.id.cvGenericValues);
+        tituloEnum = (TextView) itemView.findViewById(R.id.textViewTituloEnum);
+        valorEnum= (TextView) itemView.findViewById(R.id.textViewValorEnum);
+        descripcionEnum =(TextView) itemView.findViewById(R.id.textViewDescripcionEnum);
+    }
+
+}
+
